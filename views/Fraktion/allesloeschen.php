4feb2cbe40e94bb6dd9c954ec038f3e43cedf858
Robin Krause <Lrt71793@gmx.de>
2018-10-01 22:47:40 +0200

    2 Leerzeilen oben hinzugefügt

--- views/Fraktion/allesloeschen.php
+++ views/Fraktion/allesloeschen.php
@@ -2,7 +2,7 @@
 
 ?>
 
-
+<br><br>
         <form class="form-inline" action="<?php echo URL;?>/Fraktion/<?php echo $this->Land;?>" method="POST">
         <button class="btn btn-outline-danger" name="wirklichallesloeschen" value="true">wirklich alles löschen?</button>
         <button class="btn btn-outline-success" name="wirklichnichtsloeschen" value="true">nein nichts löschen!</button>
--- views/Fraktion/bestaetigen.php
+++ views/Fraktion/bestaetigen.php
@@ -2,7 +2,7 @@
 
 ?>
 
-
+<br><br>
         <form class="form-inline" action="<?php echo URL;?>/Fraktion/<?php echo $this->Land;?>" method="POST">
         <button class="btn btn-outline-danger" name="wirklichBestaetigen" value="true">wirklich bestätigen?</button>
         <button class="btn btn-outline-success" name="wirklichnichtBestaetigen" value="true">nein nichts bestätigen!</button>
